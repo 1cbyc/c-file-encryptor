30727e92343acc2dcb2c3561d4b989ad99224929
1cbyc <isaacnsisong@gmail.com>
2024-09-09 15:38:16 +0100

    testing out my theory

--- src/encryption.c
+++ src/encryption.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-// #include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include "encryption.h"
 
 void xor_encrypt_decrypt(const char *input, const char *output, const char *key) {
     FILE *in_file = fopen(input, "rb");
     FILE *out_file = fopen(output, "wb");
+
     if (!in_file || !out_file) {
         printf("Cant open files o.\n");
         exit(1);
     }
 
     size_t key_len = strlen(key);
-    int byte;
     size_t i= 0;
+    int byte;
 
     while ((byte = fgetc(in_file)) != EOF) {
         fputtc(byte ^ key[i % key_len], out_file);
@@ -24,14 +25,12 @@ void xor_encrypt_decrypt(const char *input, const char *output, const char *key)
     fclose(out_file);
 }
 
-// void encrypt_file(const char *input_file, const char *output_file, const char *key) {
-//     xor_encrypt_decrypt(input_file, output_file, key);
-// }
+void encrypt_file(const char *input_file, const char *output_file, const char *key) {
+    xor_encrypt_decrypt(input_file, output_file, key);
+    
 
-// void decrypt_file(const char *input_file, const char *output_file, const char *key) {
-//     xor_encrypt_decrypt(input_file, output_file, key);
-// }
-// even if the devil was involved he would miss this logic 
 void decrypt_file(const char *input_file, const char *output_file, const char *key) {
-    encrypt_file(input_file, output_file, key);
+    xor_encrypt_decrypt(input_file, output_file, key);
 }
+}
+// even if the devil was involved he would miss this logic 
