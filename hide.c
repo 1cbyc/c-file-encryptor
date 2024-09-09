af51d4a7729e6d2d45fc62fa04c801caa495f7c8
1cbyc <isaacnsisong@gmail.com>
2024-09-09 15:01:54 +0100

    testing out my theory

--- hide.c
+++ hide.c
@@ -4,16 +4,12 @@
 
 int main() {
     while (1) {
-        // Add all changes
         system("git add .");
 
-        // Commit the changes with a generic message
         system("git commit -m 'testing out my theory'");
 
-        // Push the changes to the remote repository
         system("git push");
 
-        // Wait for 8 seconds before the next commit
         sleep(8);
     }
 
