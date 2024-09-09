90000a9fc958e6f1d920b150445b7a5b277e8664
1cbyc <isaacnsisong@gmail.com>
2024-09-09 14:59:37 +0100

    testing out my theory

--- src/encryption.c
+++ src/encryption.c
@@ -9,6 +9,6 @@ void xor_encrypt_decrypt(const char *input, const char *output, const char *key)
     FILE *out_file = fopen(output, "wb");
 
     if (!in_file || !out_file) {
-        printf("Cant open files o.")
+        printf("Cant open files o.\n")
     }
 }
