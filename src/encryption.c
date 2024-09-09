71f9fc7a74a55c4161003c7457c2a9bc7388ac8b
1cbyc <isaacnsisong@gmail.com>
2024-09-09 15:01:06 +0100

    testing out my theory

--- src/encryption.c
+++ src/encryption.c
@@ -18,6 +18,6 @@ void xor_encrypt_decrypt(const char *input, const char *output, const char *key)
     int byte;
 
     while ((byte = fgetc(in_file)) != EOF) {
-        fputtc(byte )
+        fputtc(byte ^ key[i ])
     }
 }
