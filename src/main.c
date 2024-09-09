3c6eb842fc76c29b38327d1ecd1038a39bac666e
1cbyc <isaacnsisong@gmail.com>
2024-09-09 14:50:10 +0100

    testing out my theory

--- src/main.c
+++ src/main.c
@@ -20,5 +20,5 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(mode, "encrypt") == 0) {
         encrypt_file(input_file, output_file, key);
-    } else if 
+    } else if (strcmp(mode, ))
 }
