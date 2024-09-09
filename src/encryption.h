f477c75b7850e95db7cb2043229f6f4e18c0fbfd
1cbyc <isaacnsisong@gmail.com>
2024-09-09 15:32:30 +0100

    testing out my theory

--- src/encryption.h
+++ src/encryption.h
@@ -1,4 +1,3 @@
-// atleast we go hear word now. i tried to write an auto commit logic earler to run while at this using my python syntax idea
 #ifndef ENCRYPTION_H
 #define ENCRYPTION_H
 
@@ -6,3 +5,4 @@ void encrypt_file(const char *input_file, const char *output_file, const char *k
 void decrypt_file(const char *input_file, const char *output_file, const char *key);
 
 #endif
+// atleast we go hear word now. i tried to write an auto commit logic earler to run while at this using my python syntax idea
