fc6c7acd4033edb98163b5df57e074ca7b8dbed9
1cbyc <isaacnsisong@gmail.com>
2024-10-13 15:49:03 +0100

    testing out my theory

--- src/encryptor.c
+++ src/encryptor.c
@@ -49,6 +49,8 @@ void encrypt_file(const char *input_file, const char *output_file, const unsigne
     unsigned char outbuf[BLOCK_SIZE + EVP_MAX_BLOCK_LENGTH];
     int outlen, bytes_read;
 
+    
+
 
     AES_KEY aes_key;
     AES_set_encrypt_key(key, 256, &aes_key);
