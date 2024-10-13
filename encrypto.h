2f923a9df3bb186d7b79a66e4acaeeb1ed778f7e
1cbyc <isaacnsisong@gmail.com>
2024-10-13 15:32:50 +0100

    testing out my theory

---
+++ encrypto.h
@@ -0,0 +1,16 @@
+#ifndef ENCRYPTOR_H
+#define ENCRYPTOR_H
+
+#include <stdio.h>
+#include <openssl/aes.h>
+#include <openssl/sha.h>
+#include <openssl/rand.h>
+
+void encrypt_file(const char *input_file, const char *output_file, const unsigned char *key);
+void decrypt_file(const char *input_file, const char *output_file, const unsigned char *key);
+void generate_key_from_password(const char *password, unsigned char *key);
+void sha256_hash_file(const char *filename, unsigned char *hash);
+void progress_feedback(long processed, long total);
+int verify_integrity(const char *input_file, const unsigned char *original_hash);
+
+#endif
